e8be839b60cd2bb0816a760ba7781bf74ed3e781
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-03-29 17:02:30 +0530

    Added JS

---
+++ script.js
@@ -0,0 +1,26 @@
+const tagsEl = document.getElementById('tags');
+const textarea = document.getElementById('textarea');
+
+textarea.focus();
+
+textarea.addEventListener('keyup', (e) => {
+  createTags(e.target.value);
+});
+
+function createTags(input) {
+  // Creating an Array
+  const tags = input
+    .split(',')
+    .filter((tag) => tag.trim() !== '')
+    .map((tag) => tag.trim());
+  // Emptying previous choices
+  tagsEl.innerHTML = '';
+  tags.forEach((tag) => {
+    // Crating an Element
+    const tagEl = document.createElement('span');
+    tagEl.classList.add('tag');
+    tagEl.innerText = tag;
+    // Adding element to DOM
+    tagsEl.appendChild(tagEl);
+  });
+}
